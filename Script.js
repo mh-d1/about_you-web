f977f1dd8462787200b90597cedf669d563104bb
mmhdhnfii-byte <mmhdhnfii@gmail.com>
2026-01-30 19:50:02 +0700

    Update Script.js

--- Script.js
+++ Script.js
@@ -15,7 +15,12 @@ function startAudio() {
 function toggleContent(id) {
   const box = document.getElementById(id);
 
-  
+  if (!box) return;
+
+  if (box.style.display === "block") {
+    box.style.display = "none";
+  } else {
+    box.style.display = "block";
 
     // PLAY AUDIO hanya untuk tombol pertama (c1)
     if (id === "c1") {
@@ -23,7 +28,7 @@ function toggleContent(id) {
     }if (box.style.display === "block") {
   // tutup konten
   if (id === "c1") {
-    audio.pause();           // hentikan audio
+    audio.pause().catch(...);           // hentikan audio
     audio.currentTime = 0;   // reset ke awal
   }
 } else {
